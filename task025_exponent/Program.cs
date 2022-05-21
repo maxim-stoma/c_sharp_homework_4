d018828ad0e1076a360584f6cab23456adbdc42b
maxim_stoma <maksim.stoma@mail.ru>
2022-05-21 14:46:44 +0300

    task025 solved

---
+++ task025_exponent/Program.cs
@@ -0,0 +1,33 @@
+﻿void Exponentiation()
+{
+    Console.Write("Введите число, которое нужно возвести в степень: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите степень: ");
+    int power = Convert.ToInt32(Console.ReadLine());
+    
+    int result = 1;
+
+    if (power == 0)
+    {
+        Console.WriteLine($"Число {number} в степени {power} равно {result}");
+    }
+    else if (power > 0)
+    {
+        for (int i = 0; i != power; i++)
+        {
+        result = result * number;
+        }
+        Console.WriteLine($"Число {number} в степени {power} равно {result}");
+    }
+    else
+    {
+        for (int i = 0; i != power; i++)
+        {
+        result = result * number;
+        }
+        Console.WriteLine($"Число {number} в степени {power} равно {result}");
+    }
+}
+
+Exponentiation();
+Console.ReadLine();
