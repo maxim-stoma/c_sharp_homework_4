652ac02c19b43ffd5c2a4d3ad46dc11ead9f93e7
maxim_stoma <maksim.stoma@mail.ru>
2022-05-21 15:50:26 +0300

    task027 solved

---
+++ task027_sumOfDigits/Program.cs
@@ -0,0 +1,18 @@
+﻿void GetSumOfDigits()
+{
+    Console.Write("Введите число: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    int div = num;
+    int mult;
+    int sum = 0;
+
+    for (num = div; div != 0; div = div / 10)
+    {
+        mult = div % 10;
+        sum = sum + mult;
+    }
+    Console.WriteLine($"Сумма цифр числа {num} равна {sum}");
+}
+
+GetSumOfDigits();
+Console.ReadLine();
