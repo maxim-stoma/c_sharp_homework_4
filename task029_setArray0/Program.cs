481bd126e36ddd6f20b2b213d3d163c8bbf6cb18
maxim_stoma <maksim.stoma@mail.ru>
2022-05-21 19:59:35 +0300

    task029 solved

---
+++ task029_setArray0/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Статичный массив случайных чисел от 0 до 99: ");
+
+void SetArray(int[] variety)
+{
+    int length = variety.Length;
+    int index = 0;
+
+    for (index = 0; index < length; index++)
+    {
+        variety[index] = new Random().Next (0, 100);
+    }
+}
+
+void PrintArray(int[] multitude)
+{
+    int dimention = multitude.Length;
+    int pos = 0;
+
+    Console.Write("[ ");
+    for (pos = 0; pos < dimention; pos++)
+    {
+        Console.Write(multitude[pos]);
+        if (pos != dimention - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write(" ]");
+}
+
+int[] array = new int[8];
+
+SetArray(array);
+PrintArray(array);
+Console.ReadLine();
